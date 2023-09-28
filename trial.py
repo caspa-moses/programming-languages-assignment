e83a6191d6c0f2fa44cbcb21dbecc4c465925234
caspa-moses <118312143+caspa-moses@users.noreply.github.com>
2023-09-28 23:08:05 +0300

    Add files via upload

---
+++ trial.py
@@ -0,0 +1,11 @@
+    # MOSES ODENY
+    # SCT211-0099/2022
+
+name = input("enter your name ")
+num1 = int(input("enter the first number "))
+num2 = int(input("enter the second number "))
+
+print("hello",name, "the sum of",num1, 'and', num2, "is" ,num1 + num2)
+
+
+
